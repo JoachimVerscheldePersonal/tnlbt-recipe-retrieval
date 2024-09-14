49bcda1f4de022a020bc947d2ca7614af810d04f
Joachim Verschelde <joachim.verschelde@gmail.com>
2024-09-14 16:44:36 +0200

    feat: image encoder

--- src/domain.py
+++ src/domain.py
@@ -3,7 +3,12 @@ from torch import rand
 
 class RecipeEncoder():
 
-    def __init__(self, embedding_size = 512, attention_heads = 8):
+    def __init__(self, embedding_size = 512, attention_heads = 8) -> None:
         self.encoder_layer = TransformerEncoderLayer(d_model=embedding_size, n_head=attention_heads)
         src = rand(10,32,embedding_size)
         out = self.encoder_layer(src)
+
+class ImageEncoder():
+
+    def __init__(self) -> None:
+        pass
